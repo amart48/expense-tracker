fd96e2ddbe2ed428d890457326823e521f53fd5a
mart2164 <mart2164@purdue.edu>
2024-05-06 21:41:27 -0400

    made changes

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,22 +1,20 @@
-import React, { useContext } from 'react'
-import { GlobalContext } from '../context/GlobalState'
+import React, { useContext } from 'react';
+import Transaction from './Transaction'; 
+import { GlobalContext } from '../context/GlobalState';
 
 const TransactionList = () => {
   const { transactions } = useContext(GlobalContext);
-
-  
 
   return (
     <div>
       <h3>History</h3>
       <ul className="list"> 
-        {transactions.map(transaction => (<li className="minus">
-            {transaction.text} <span>-400</span><button className='delete-btn'>x</button>
-        </li>)) }
-        
+        {transactions.map(transaction => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
       </ul>
     </div>
   )
 }
 
-export default TransactionList
+export default TransactionList;
